cc4dc6aa896016478e1371868533d7ff8aa84509
Luuk van den Maagdenberg <luuk.van.den.maagdenberg@sue.nl>
2022-06-12 13:01:59 +0200

    rewrote the tests and added more test cases

--- src/health/checker_test.go
+++ src/health/checker_test.go
@@ -3,30 +3,15 @@ package health
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"github.com/suecodelabs/cnfuzz/src/log"
+	"go.uber.org/zap"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
-
-func TestNewChecker(t *testing.T) {
-	c := NewChecker()
-	assert.NotNil(t, c.checkers)
-}
-
-func TestRegisterHealthyCheck(t *testing.T) {
-	c := NewChecker()
-	check := fakeHealthyCheck{}
-	checkName := "test-checker"
-	c.RegisterCheck(checkName, check)
-	status := c.IsHealthy()
-	assert.True(t, status.IsHealthy)
-	assert.Equal(t, HealthyStatus, status.Info[StatusKey])
-	var testCheckMap map[string]any
-	testCheckMap = status.Info[checkName].(map[string]any)
-	assert.Equal(t, HealthyStatus, testCheckMap[StatusKey])
-}
 
 type fakeHealthyCheck struct {
 }
@@ -35,19 +20,6 @@ func (fakeHealthyCheck) CheckHealth(context context.Context) Health {
 	h := NewHealth(true)
 	h.Info[StatusKey] = HealthyStatus
 	return h
-}
-
-func TestRegisterUnhealthyCheck(t *testing.T) {
-	c := NewChecker()
-	check := fakeUnhealthyCheck{}
-	checkName := "test-checker"
-	c.RegisterCheck(checkName, check)
-	status := c.IsHealthy()
-	assert.False(t, status.IsHealthy)
-	assert.Equal(t, UnHealthyStatus, status.Info[StatusKey])
-	var testCheckMap map[string]any
-	testCheckMap = status.Info[checkName].(map[string]any)
-	assert.Equal(t, UnHealthyStatus, testCheckMap[StatusKey])
 }
 
 type fakeUnhealthyCheck struct {
@@ -59,24 +31,90 @@ func (fakeUnhealthyCheck) CheckHealth(context context.Context) Health {
 	return h
 }
 
-func TestHttpHealthCheck(t *testing.T) {
-	// TODO create more test cases
+func TestNewChecker(t *testing.T) {
 	c := NewChecker()
+	assert.NotNil(t, c.checkers)
+}
 
-	req := httptest.NewRequest(http.MethodGet, "/health", nil)
-	w := httptest.NewRecorder()
-	c.Health(w, req)
-	res := w.Result()
-	defer res.Body.Close()
+func TestRegisterCheck(t *testing.T) {
+	cases := []struct {
+		check          ICheck
+		checkName      string
+		isHealthy      bool
+		expectedStatus string
+	}{
+		{fakeUnhealthyCheck{}, "unhealthy-test-check", false, UnHealthyStatus},
+		{fakeHealthyCheck{}, "healthy-test-check", true, HealthyStatus},
+		{nil, "nil-test-check", true, HealthyStatus},
+	}
 
-	assert.Equal(t, http.StatusOK, res.StatusCode)
+	for _, c := range cases {
+		checker := NewChecker()
 
-	data, err := ioutil.ReadAll(res.Body)
-	if assert.NoError(t, err, "http health request shouldn't error out") {
-		var payload map[string]interface{}
-		err = json.Unmarshal(data, &payload)
-		if assert.NoError(t, err, "returned json is invalid") {
-			assert.Equal(t, HealthyStatus, payload[StatusKey])
+		var status Health
+		if c.check == nil {
+			// test if the logger logs the failed health check registration
+
+			logs := log.SetupLogsCapture()
+
+			checker.RegisterCheck(c.checkName, c.check)
+			status = checker.IsHealthy()
+
+			entry := logs.All()[0]
+			assert.Equal(t, zap.WarnLevel, entry.Level, "expected a message with the 'warn' log level got %s", entry.Level)
+			assert.Equal(t, fmt.Sprintf("failed to register %s health check, because it doesn't contain a check function", c.checkName), entry.Message, "got an unexpected log message while registering a nil health check")
+		} else {
+			// in this scenario we don't expect any log messages, so just register the health check as usual
+			checker.RegisterCheck(c.checkName, c.check)
+			status = checker.IsHealthy()
+		}
+
+		assert.Equal(t, c.isHealthy, status.IsHealthy, "expected different health state")
+		assert.Equal(t, c.expectedStatus, status.Info[StatusKey], "expected different health status key")
+		if c.check != nil {
+			var testCheckMap map[string]any
+			testCheckMap = status.Info[c.checkName].(map[string]any)
+			assert.Equal(t, c.expectedStatus, testCheckMap[StatusKey])
 		}
 	}
+
+}
+
+func TestHttpHealthCheck(t *testing.T) {
+	cases := []struct {
+		check              ICheck
+		checkName          string
+		expectedStatus     string
+		expectedStatusCode int
+	}{
+		{fakeUnhealthyCheck{}, "unhealthy-test-check", UnHealthyStatus, http.StatusServiceUnavailable},
+		{fakeHealthyCheck{}, "healthy-test-check", HealthyStatus, http.StatusOK},
+		{nil, "nil-test-check", HealthyStatus, http.StatusOK},
+	}
+
+	for _, c := range cases {
+		// TODO create more test cases
+		checker := NewChecker()
+
+		checker.RegisterCheck(c.checkName, c.check)
+
+		req := httptest.NewRequest(http.MethodGet, "/health", nil)
+		w := httptest.NewRecorder()
+		checker.Health(w, req)
+		res := w.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, c.expectedStatusCode, res.StatusCode, "http request didn't return the expected status code")
+
+		data, err := ioutil.ReadAll(res.Body)
+		if assert.NoError(t, err, "http health request shouldn't error out") {
+			var respObj map[string]interface{}
+			err = json.Unmarshal(data, &respObj)
+			if assert.NoError(t, err, "returned json is invalid") {
+				assert.Equal(t, c.expectedStatus, respObj[StatusKey])
+			}
+		}
+
+	}
+
 }
